a1c3a453744feaa809c5e7dbe06ddfd6a475045c
asaskiap <alannapfeiffer@gmail.com>
2020-12-19 12:58:43 +0100

    missing styling

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
@@ -19,7 +20,40 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+    res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+    punkAPI
+        .getBeers()
+        .then(beersFromApi =>
+            res.render('beers', {
+                beers: beersFromApi
+            })
+        )
+        .catch(error => console.log(error));
+});
+
+app.get('/random', (req, res) => {
+    punkAPI
+        .getRandom()
+        .then(responseFromAPI => {
+            res.render('random', {
+                beer: responseFromAPI
+            });
+        })
+        .catch(error => console.log(error));
+});
+
+app.get(`/beers/:id`, (req, res) => {
+    punkAPI
+        .getBeer(req.params.id)
+        .then(responseFromAPI => {
+            res.render('single', {
+                beer: responseFromAPI
+            });
+        })
+        .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
